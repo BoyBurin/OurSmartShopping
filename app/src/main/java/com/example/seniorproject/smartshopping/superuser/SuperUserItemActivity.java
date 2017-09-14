463c878b0f39501d334913fd63e2f484eac7e751
boyburin <boy-burin@hotmail.com>
2017-09-14 16:09:41 +0700

    SuperUserItemActivity can upload photo

--- app/src/main/java/com/example/seniorproject/smartshopping/superuser/SuperUserItemActivity.java
+++ app/src/main/java/com/example/seniorproject/smartshopping/superuser/SuperUserItemActivity.java
@@ -1,5 +1,7 @@
 package com.example.seniorproject.smartshopping.superuser;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +16,14 @@ import android.widget.TextView;
 import com.example.seniorproject.smartshopping.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class SuperUserItemActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
     private DatabaseReference mDatabaseRef;
+    private StorageReference mStorageRef;
 
     private TextView tvShowBarcode;
     private Button btnAddBarcode;
@@ -26,6 +31,13 @@ public class SuperUserItemActivity extends AppCompatActivity {
     private EditText edtRetailPrice;
     private EditText edtType;
     private EditText edtUnit;
+
+    private Button btnUploadImage;
+    private TextView tvURI;
+
+    private Uri selectedImageUri;
+
+    final static int RC_PHOTO_PICKER =  2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +60,14 @@ public class SuperUserItemActivity extends AppCompatActivity {
         edtRetailPrice = (EditText) findViewById(R.id.edtRetailPrice);
         edtType = (EditText) findViewById(R.id.edtType);
         edtUnit = (EditText) findViewById(R.id.edtUnit);
+
+        btnUploadImage = (Button) findViewById(R.id.btnUploadImage);
+        tvURI = (TextView) findViewById(R.id.tvURI);
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        btnUploadImage.setOnClickListener(uploadImageListener);
     }
 
     @Override
@@ -64,6 +84,34 @@ public class SuperUserItemActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
 
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_PHOTO_PICKER){
+            if(resultCode == RESULT_OK){
+                Uri selectedImageUri = data.getData();
+                this.selectedImageUri = selectedImageUri;
+                tvURI.setText(selectedImageUri.getLastPathSegment());
+                tvURI.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    /***********************************************************************************************
+     ************************************* Listener variables ********************************************
+     ***********************************************************************************************/
+
+    final View.OnClickListener uploadImageListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/jpeg");
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+            startActivityForResult(Intent.createChooser(intent, "Complete action using"),
+                    RC_PHOTO_PICKER);
         }
     };
 }
